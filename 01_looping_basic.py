3a88ea44e914961ad4b9a784f3dda9789aba7979
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-02 17:56:30 +1300

    Update 01_looping_basic.py

--- 01_looping_basic.py
+++ 01_looping_basic.py
@@ -1,8 +1,41 @@
-valid = False
-while not valid:
-	response = float(input("Enter a number:  "))
-	if response > 0:
-		valid = True 
-	else:
-		print("Please enter value more than 0\n")
+# checks input is a number more than 0
+
+def num_check(question):
+	valid = False
+	while not valid:
+		error = "Please enter value more than 0\n"
+		try:
+			# ask user to enter a number
+
+			response = float(input(question))
+			
+			# check if the number is more than 0
+			
+			if response > 0:
+				return response  
+
+			#outputs error if the input is invalid
+
+			else:
+				print(error)
+		except ValueError: 
+			print(error)
 	
+
+
+
+
+
+
+width = num_check("Width:")
+height = num_check("height:")
+print()
+print("width",width)
+print("height",height)
+print()
+area = width*height 
+perimeter = 2*(width + height)
+print("Perimeter:", perimeter, "units")
+print("area:", area, "square units")
+
+
